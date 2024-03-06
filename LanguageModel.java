b6ce911ab9b4fc6425604162efaddcec8b8ed906
orital8 <ori.tal01@post.runi.ac.il>
2024-03-06 15:30:08 +0200

    firstTry

--- LanguageModel.java
+++ LanguageModel.java
@@ -34,30 +34,30 @@ public class LanguageModel {
     /** Builds a language model from the text in the given file (the corpus). */
 	public void train(String fileName) {
         String window = "";
-        char chr = ' ' ;
+        char c = ' ' ;
 
         In in = new In(fileName);
 
         for (int i = 0; i < windowLength; i++)
         {
-            chr = in.readChar();
-            window += chr;
+            c = in.readChar();
+            window += c;
         }
 
         while (!in.isEmpty())
         {
-            chr = in.readChar();
+            c = in.readChar();
             if (CharDataMap.containsKey(window))
             {
-                CharDataMap.get(window).update(chr);
+                CharDataMap.get(window).update(c);
             }
             else
             {
                 List probs = new List();
-                probs.addFirst(chr);
+                probs.addFirst(c);
                 CharDataMap.put(window, probs);
             }
-            window = window.substring(1) + chr;
+            window = window.substring(1) + c;
         }
 
         for (List probs : CharDataMap.values())
@@ -67,12 +67,35 @@ public class LanguageModel {
     // Computes and sets the probabilities (p and cp fields) of all the
 	// characters in the given list. */
 	public void calculateProbabilities(List probs) {				
-		// Your code goes here
-	}
+        int countch = 0;
+        Node current = probs.getFirstNode();
+        while (current != null)
+        {
+            countch += current.cp.count;
+            current = current.next;
+        }
+
+        double currentcp = 0;
+        current = probs.getFirstNode();
+        while (current != null)
+        {
+            current.cp.p = (double) current.cp.count / countch;
+            currentcp += (double) current.cp.count / countch;;
+            current.cp.cp = currentcp;
+            current = current.next;
+        }
+    	}
 
     // Returns a random character from the given probabilities list.
 	public char getRandomChar(List probs) {
-		// Your code goes here
+        //double r = Math.random();
+        Node current = probs.getFirstNode();
+        double r = randomGenerator.nextDouble();
+        while (r > current.cp.cp)
+        {
+            current = current.next;
+        }
+        return current.cp.chr;
 	}
 
     /**
@@ -83,13 +106,39 @@ public class LanguageModel {
 	 * @return the generated text
 	 */
 	public String generate(String initialText, int textLength) {
-		// Your code goes here
-	}
+        String window = "";
+        String text = initialText;
+        char chr;
+
+        if (windowLength > initialText.length() || initialText.length() >= textLength)
+        {
+            return initialText;
+        }
+        else
+        {
+            window = initialText.substring(initialText.length() - windowLength);
+            while (text.length() - windowLength < textLength) 
+            {
+                if (CharDataMap.containsKey(window))
+                {
+                    chr = getRandomChar(CharDataMap.get(window));
+                    text += chr;
+                    window = window.substring(1) + chr;
+                }
+                else
+                {
+                    return text;
+                }
+            }
+            return text;
+        }
+    }
 
     /** Returns a string representing the map of this language model. */
 	public String toString() {
-		StringBuilder str = new StringBuilder();
-		for (String key : CharDataMap.keySet()) {
+        StringBuilder str = new StringBuilder();
+		for (String key : CharDataMap.keySet())
+        {
 			List keyProbs = CharDataMap.get(key);
 			str.append(key + " : " + keyProbs + "\n");
 		}
